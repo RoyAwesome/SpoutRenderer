4e4e8b32e38e5192ff446afd3c1b50aa287ec174
RoyAwesome <madKylegar@gmail.com>
2012-01-25 17:21:40 -0800

    Added URILocation for resources.

--- src/main/java/org/spout/client/SpoutClient.java
+++ src/main/java/org/spout/client/SpoutClient.java
@@ -1,9 +1,16 @@
 package org.spout.client;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.SocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -55,100 +62,100 @@ import org.spout.api.util.Color;
 import static org.lwjgl.opengl.GL11.*;
 
 public class SpoutClient implements Client {
-	
+
 	public static boolean glInfo = true;
-	
+
 	public void start() throws InterruptedException {
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
-			
+
 			Display.create();
 
 			if(glInfo){
-				
+
 				System.out.println("OpenGL Information");
 				System.out.println(GL11.glGetString(GL11.GL_VENDOR));
 				System.out.println(GL11.glGetString(GL11.GL_RENDER));
 				System.out.println(GL11.glGetString(GL11.GL_VERSION));
 				System.out.println(GL11.glGetString(GL20.GL_SHADING_LANGUAGE_VERSION));
 				System.out.println(GL11.glGetString(GL11.GL_EXTENSIONS));
-		
+
 			}
-			
+
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
-		
+
+
 		Shader shader = new BasicShader();
 		//Shader shader = new ClientShader("src/main/resources/vBasicLight.glsl", "src/main/resources/fBasicLight.glsl");
-		
-	
+
+
 		PrimitiveBatch batch = new PrimitiveBatch();
 		batch.getRenderer().setShader(shader);
-		
+
 		Color col = new Color(0.0f, .5f, 0.0f);
 		Color col2 = new Color(0.0f, 0.0f, 0.5f);
 		Color col3 = new Color(0.5f, 0.0f, 0.0f);
-		
+
 		Color ambientColor = new Color(.5f, .5f, .5f);
-		
+
 		Vector3 lightDirection = new Vector3(1.0f, 0, 0);
 		Color lightColor = new Color(.9f, 0.0f, 0.0f);
-		
+
 		shader.setUniform("ambientColor", ambientColor);
 		shader.setUniform("lightDirection", lightDirection);
 		shader.setUniform("lightColor", lightColor);
-		
-	
+
+
 		Matrix perspective = Matrix.createPerspective(60, 4.f/3.f, .1f, 100f);
 		shader.setUniform("Projection", perspective);
-		
-		
+
+
 		PositionColor[] corners = { new PositionColor(Vector3.ZERO, col3) , new PositionColor(Vector3.UNIT_Y, col), new PositionColor(new Vector3(0,1,1), col), new PositionColor(Vector3.UNIT_Z, col),
 				new PositionColor(Vector3.UNIT_X, col3), new PositionColor(new Vector3(1,1,0), col2), new PositionColor(Vector3.ONE, col2), new PositionColor(new Vector3(1, 0, 1), col2)};
-		
-		
-		
-		
+
+
+
+
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glEnable(GL_DEPTH_TEST);
-		
+
 		int ticks = 0;
 		while (!Display.isCloseRequested()) {
 			long time  = System.currentTimeMillis();
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			
+
 			double x = 5 * Math.sin(Math.toRadians(ticks));
 			double z = 5 * Math.cos(Math.toRadians(ticks));
 			double y = 5 * Math.sin(Math.toRadians(ticks));
-			
+
 			Matrix view = Matrix.createLookAt(new Vector3(x,y,z), Vector3.ZERO, Vector3.Up);
-			
+
 			if(shader instanceof BasicShader){
 				((BasicShader) shader).setViewMatrix(view);
 			}else{
 				shader.setUniform("View", view);		
 			}
 			batch.getRenderer().setShader(shader);
-			
-		
 
-			
+
+
+
 			batch.begin();
 			batch.addQuad(corners[0], corners[1], corners[2], corners[3]); //draws
-			
+
 			batch.addQuad(corners[7], corners[6], corners[5], corners[4]);
-			
+
 			batch.addQuad(corners[3], corners[2], corners[6], corners[7]);
-			
+
 			batch.addQuad(corners[4], corners[5], corners[1], corners[0]); //draws
 			batch.addQuad(corners[1], corners[5], corners[6], corners[2]);
 			batch.addQuad(corners[4], corners[0], corners[3], corners[7]);
 			batch.end();
-			
-			
+
+
 			batch.draw();
 
 			Display.update();
@@ -160,16 +167,29 @@ public class SpoutClient implements Client {
 				long total = System.currentTimeMillis() - time;
 				System.out.println("fps: " + (1.0/total) * 1000);
 			}
-			
-			
+
+
 		}
 
 		Display.destroy();
 	}
 
+
+	public static int n = 10;
+
 	public static void main(String[] argv) throws InterruptedException, LWJGLException {
-		
-		
+		try {
+			URI url = new URI("texture://Vanilla/path/to/texture.png");
+			System.out.println(url.getHost());
+			System.out.println(url.getPath());
+			System.out.println(url.getScheme());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
 		SpoutClient displayExample = new SpoutClient();
 		Spout.setGame(displayExample);
 		displayExample.start();
@@ -195,7 +215,7 @@ public class SpoutClient implements Client {
 	 */
 	private WorldGenerator defaultGenerator = null;
 
-		/**
+	/**
 	 * The security manager
 	 * TODO - need to integrate this
 	 */
@@ -205,7 +225,7 @@ public class SpoutClient implements Client {
 	 * The plugin manager for the server
 	 */
 	private CommonPluginManager pluginManager = new CommonPluginManager(this, securityManager, 0.0);
-/**
+	/**
 	 * The logger for this class.
 	 */
 	public static final Logger logger = Logger.getLogger("Minecraft");
@@ -235,17 +255,17 @@ public class SpoutClient implements Client {
 	 * A list of all players who can log onto this server, if using a whitelist.
 	 */
 	private List<String> whitelistedPlayers = new ArrayList<String>();
-	
+
 	/**
 	 * A list of all players who can not log onto this server.
 	 */
 	private List<String> bannedPlayers = new ArrayList<String>();
-	
+
 	/**
 	 * A list of all operators.
 	 */
 	private List<String> operators = new ArrayList<String>();
-	
+
 	/**
 	 * A folder that holds all of the world data folders inside of it. By default, it does not exist ('.'), meant for organizational purposes.
 	 */
@@ -260,10 +280,10 @@ public class SpoutClient implements Client {
 	 * The event manager.
 	 */
 	private final EventManager eventManager = new SimpleEventManager();
-	
+
 	private final ConcurrentMap<SocketAddress, BootstrapProtocol> bootstrapProtocols = new ConcurrentHashMap<SocketAddress, BootstrapProtocol>();
-	
-	
+
+
 	public String getName() {
 		return name;
 	}
@@ -299,7 +319,7 @@ public class SpoutClient implements Client {
 
 	public void broadcastMessage(String message) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public PluginManager getPluginManager() {
@@ -312,7 +332,7 @@ public class SpoutClient implements Client {
 
 	public void processCommand(CommandSource source, String commandLine) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public File getUpdateFolder() {
@@ -355,12 +375,12 @@ public class SpoutClient implements Client {
 
 	public void save(boolean worlds, boolean players) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public File getWorldFolder() {
@@ -405,7 +425,7 @@ public class SpoutClient implements Client {
 
 	public void setDefaultGenerator(WorldGenerator generator) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Scheduler getScheduler() {
@@ -415,7 +435,7 @@ public class SpoutClient implements Client {
 
 	public void addRecipe(Recipe recipe) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Recipe getRecipe(Plugin plugin, String recipe) {
@@ -485,12 +505,12 @@ public class SpoutClient implements Client {
 
 	public void setCamera(Point loc) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void detachCamera(boolean detach) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public boolean isCameraDetached() {
